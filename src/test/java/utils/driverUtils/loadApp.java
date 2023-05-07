ec4f2d5d9efb1ebba962800cc0c3649c6d1dfa6a
Ayobamee <ayobami.elutade@com>
2023-05-07 16:22:33 +0100

    updated reports

--- src/test/java/utils/driverUtils/loadApp.java
+++ src/test/java/utils/driverUtils/loadApp.java
@@ -1,7 +1,7 @@
 package utils.driverUtils;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -27,6 +27,7 @@ public class loadApp {
 
             ChromeOptions option = new ChromeOptions();
             option.addArguments("--remote-allow-origins=*");
+//            option.setHeadless(true);
 
             WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver(option);
